c53ea63b5c228efcbc526b122e81061795ec9019
Fabio <fabio@bytheway.com.hk>
2018-11-26 00:50:55 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-const express = require('express')
-const PORT = process.env.PORT || 5000
-var app = express()
-/*
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-*/
-app.get('/notes', function(req, res) {
-	res.json({notes: "This is your notebook"})
+const TelegramBot 	= require('node-telegram-bot-api')
+const PORT			= process.env.PORT || 5000
+const token			= '529343186:AAEGqz0kDM-AWrDUnGWOgXcw8w88e7Kf-DY'
+
+var bot 			= new TelegramBot(token, {polling: true})
+
+bot.onText(/\/start/, function(msg) {
+	var chatId	= msg.chat.id
+	var resp	= 'Hello'
+	bot.sendMessage(chatId, resp)
 })
 
-app.get('/test', function(req, res) {
-	res.json({test: "This is test"})
+bot.onText(/\/cal (.+)/, function (msg, match) {
+	var fromId	= msg.from.id
+	var resp	= match[1].replace(/[^-()\d/*+.]/g, '')
+	resp		= 'Result: ' + eval(resp)
+	bot.sendMessage(fromId, resp)
 })
-
-app.listen(PORT)
