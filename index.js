2b070355cc505c418df0c86f6a2557bc26dab3d9
Fabio <fabio@bytheway.com.hk>
2018-11-27 16:53:37 +0800

    test

--- index.js
+++ index.js
@@ -14,12 +14,10 @@ bot.startWebhook('/bot' + token, null, PORT)
 bot.command('hello', (ctx) => {
 	var firstName 	= `${ctx.message.from.first_name}`
 	var lastName 	= `${ctx.message.from.last_name}`
-	console.log(firstName)
-	console.log(lastName)
-	if (firstName != ""
-		&& typeof firstName !== "undefined"
-		&& lastName != ""
-		&& typeof lastName !== "undefined") {
+	console.log(`${ctx.message.from.first_name}` != "")
+	console.log(typeof `${ctx.message.from.first_name}` !== "undefined")
+	if (`${ctx.message.from.first_name}` != "" && typeof `${ctx.message.from.first_name}` !== "undefined"
+		&& `${ctx.message.from.last_name}` != "" && typeof `${ctx.message.from.last_name}` !== "undefined") {
 			return ctx.reply('Hello ' + `${ctx.message.from.first_name} ${ctx.message.from.last_name}`)
 	} else if (firstName != ""
 		&& typeof firstName !== "undefined") {
